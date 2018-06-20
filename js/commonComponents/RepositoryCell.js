ac8aac1f32f975d0e6e1fbd17c744bb89cb48fa2
lanfairy <1264894052@qq.com>
2018-06-20 14:48:04 +0800

    repositoryCell 添加starCount  收藏按钮

--- js/commonComponents/RepositoryCell.js
+++ js/commonComponents/RepositoryCell.js
@@ -14,11 +14,23 @@ export default class RepositoryCell extends Component{
     super(props);
     this.state = {
       rowData: this.props.rowData,
+      favoriteIcon: require('../../res/images/ic_star.png')
     }
   }
+  onPressFavorite = ()=>{
 
+  };
   render(){
     let rowData = this.state.rowData;
+    let favoriteButton=
+            <TouchableHighlight
+                style={{padding:6}}
+                onPress={this.onPressFavorite} underlayColor='transparent'>
+                <Image
+                    ref='favoriteIcon'
+                    style={[{width: 22, height: 22,}]}
+                    source={this.state.favoriteIcon}/>
+            </TouchableHighlight>;
     return(
       <TouchableHighlight
         onShowUnderlay={this.props.onHighlight}
@@ -26,12 +38,18 @@ export default class RepositoryCell extends Component{
         onHideUnderlay={this.props.onUnhighlight}
       >
         <View style={[GlobalStyles.cell_container]}>
-          <Text style={styles.title}>{rowData.full_name}</Text>
-          <Text style={styles.description}>{rowData.description}</Text>
-          <View style={{flexDirection: 'row', alignItems: 'center', width: 200}}>
-            <Text style={styles.author}>Author: </Text>
-            <Image source={{uri:rowData.owner.avatar_url}} style={GlobalStyles.cell_img_owner_avatar} />
-          </View>
+            <Text style={styles.title}>{rowData.full_name}</Text>
+            <Text style={styles.description}>{rowData.description}</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={styles.author}>Author: </Text>
+                  <Image source={{uri:rowData.owner.avatar_url}} style={GlobalStyles.cell_img_owner_avatar} />
+                </View>
+                <View>
+                  <Text>{`Starts:  ${rowData.stargazers_count}`}</Text>
+                </View>
+                {favoriteButton}
+            </View>
         </View>
       </TouchableHighlight>
     );
